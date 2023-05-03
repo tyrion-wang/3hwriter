d4495f22e9c37aa1fd07a1d7a07ca2197e321937
枫子 <maple_leaf_7@msn.com>
2023-05-03 14:08:23 +0800

    first push

--- main.py
+++ main.py
@@ -269,4 +269,4 @@ def return_message():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=False, port=os.getenv("PORT", default=5000))
