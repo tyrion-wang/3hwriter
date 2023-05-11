20ff745c62c4367c31e47ac17c6616d8daab12ce
枫子 <maple_leaf_7@msn.com>
2023-05-11 19:32:57 +0800

    test

--- main.py
+++ main.py
@@ -20,64 +20,6 @@ lock = threading.Lock()  # 用于线程锁
 def index():
     return render_template('index.html')
 
-
-# 定义转写函数
-@app.route('/transcribe', methods=['POST'])
-def transcribe():
-    # 获取用户输入的文字
-    text = request.form['text']
-    # 获取用户选择的相似度
-    similarity = request.form['similarity']
-    temperature = 1.0 - float(similarity) / 10.0
-    # transcription = gpt_lib.chat(text, "围绕这个命题，生成一个800字的作文：", temperature)
-    transcription = gpt_lib.chat(text, "总结这段文本，10个字以内：", temperature)
-    # gpt_lib.chat_stream(text, "围绕这个命题，生成一个800字的作文：", temperature, socketio)
-    # gpt_lib.chat_stream(text, "总结这段文本", temperature, socketio)
-    # transcription = "123"
-    # 返回json格式的结果
-    return jsonify({'transcription': transcription.strip()})
-
-
-def gen_prompt(docs, query) -> str:
-    return f"""To answer the question please only use the Context given, nothing else. Do not make up answer, simply say 'I don't know' if you are not sure.
-Question: {query}
-Context: {[doc.page_content for doc in docs]}
-Answer:
-"""
-
-
-def prompt(query):
-    # print(query)
-    # docs = docsearch.similarity_search(query, k=4)
-    # print(docs)
-    # prompt = gen_prompt(docs, query)
-    # prompt = query
-    prompt = "写一篇2000字的关于气候变暖的论文。"
-    return prompt
-
-
-def stream(input_text):
-    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
-        {"role": "system", "content": "You're an assistant."},
-        {"role": "user", "content": f"{prompt(input_text)}"},
-    ], stream=True, max_tokens=4000, temperature=0)
-    for line in completion:
-        if 'content' in line['choices'][0]['delta']:
-            yield line['choices'][0]['delta']['content']
-
-
-@app.route('/completion', methods=['GET', 'POST'])
-def completion_api():
-    # print("111")
-    # return "test return"
-    if request.method == "POST":
-        data = request.form
-        input_text = data['input_text']
-        return Response(stream(input_text), mimetype='text/event-stream')
-    else:
-        return Response(None, mimetype='text/event-stream')
-
-
 #######################################################
 import requests
 
