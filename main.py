4eff92501bdfc5a4a2faedde0d6a54f3e7fd8187
枫子 <maple_leaf_7@msn.com>
2023-05-11 19:53:41 +0800

    test

--- main.py
+++ main.py
@@ -20,6 +20,64 @@ lock = threading.Lock()  # 用于线程锁
 def index():
     return render_template('index.html')
 
+
+# 定义转写函数
+@app.route('/transcribe', methods=['POST'])
+def transcribe():
+    # 获取用户输入的文字
+    text = request.form['text']
+    # 获取用户选择的相似度
+    similarity = request.form['similarity']
+    temperature = 1.0 - float(similarity) / 10.0
+    # transcription = gpt_lib.chat(text, "围绕这个命题，生成一个800字的作文：", temperature)
+    transcription = gpt_lib.chat(text, "总结这段文本，10个字以内：", temperature)
+    # gpt_lib.chat_stream(text, "围绕这个命题，生成一个800字的作文：", temperature, socketio)
+    # gpt_lib.chat_stream(text, "总结这段文本", temperature, socketio)
+    # transcription = "123"
+    # 返回json格式的结果
+    return jsonify({'transcription': transcription.strip()})
+
+
+def gen_prompt(docs, query) -> str:
+    return f"""To answer the question please only use the Context given, nothing else. Do not make up answer, simply say 'I don't know' if you are not sure.
+Question: {query}
+Context: {[doc.page_content for doc in docs]}
+Answer:
+"""
+
+
+def prompt(query):
+    # print(query)
+    # docs = docsearch.similarity_search(query, k=4)
+    # print(docs)
+    # prompt = gen_prompt(docs, query)
+    # prompt = query
+    prompt = "写一篇2000字的关于气候变暖的论文。"
+    return prompt
+
+
+def stream(input_text):
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[
+        {"role": "system", "content": "You're an assistant."},
+        {"role": "user", "content": f"{prompt(input_text)}"},
+    ], stream=True, max_tokens=4000, temperature=0)
+    for line in completion:
+        if 'content' in line['choices'][0]['delta']:
+            yield line['choices'][0]['delta']['content']
+
+
+@app.route('/completion', methods=['GET', 'POST'])
+def completion_api():
+    # print("111")
+    # return "test return"
+    if request.method == "POST":
+        data = request.form
+        input_text = data['input_text']
+        return Response(stream(input_text), mimetype='text/event-stream')
+    else:
+        return Response(None, mimetype='text/event-stream')
+
+
 #######################################################
 import requests
 
@@ -27,7 +85,7 @@ STREAM_FLAG = True  # 是否开启流式推送
 CHAT_CONTEXT_NUMBER_MAX = 12
 API_KEY = os.environ.get('OPENAI_API_KEY')
 
-def get_message_context1(message_history, have_chat_context, chat_with_history):
+def get_message_context(message_history, have_chat_context, chat_with_history):
     """
     获取上下文
     :param message_history:
@@ -63,7 +121,7 @@ def get_message_context1(message_history, have_chat_context, chat_with_history):
     return message_context
 
 
-def get_response_from_ChatGPT_API1(message_context, apikey):
+def get_response_from_ChatGPT_API(message_context, apikey):
     """
     从ChatGPT API获取回复
     :param apikey:
@@ -101,7 +159,7 @@ def get_response_from_ChatGPT_API1(message_context, apikey):
     return data
 
 
-def get_response_stream_generate_from_ChatGPT_API1(message_context, apikey):
+def get_response_stream_generate_from_ChatGPT_API(message_context, apikey):
     """
     从ChatGPT API获取回复
     :param apikey:
@@ -110,7 +168,7 @@ def get_response_stream_generate_from_ChatGPT_API1(message_context, apikey):
     """
     if apikey is None:
         apikey = API_KEY
-    print(apikey)
+
     header = {"Content-Type": "application/json",
               "Authorization": "Bearer " + apikey}
 
@@ -167,7 +225,7 @@ def get_response_stream_generate_from_ChatGPT_API1(message_context, apikey):
     return generate
 
 
-def handle_messages_get_response1(message, apikey, message_history, have_chat_context, chat_with_history):
+def handle_messages_get_response(message, apikey, message_history, have_chat_context, chat_with_history):
     """
     处理用户发送的消息，获取回复
     :param message: 用户发送的消息
@@ -177,21 +235,21 @@ def handle_messages_get_response1(message, apikey, message_history, have_chat_co
     :param chat_with_history: 是否连续对话
     """
     message_history.append({"role": "user", "content": message})
-    message_context = get_message_context1(message_history, have_chat_context, chat_with_history)
-    response = get_response_from_ChatGPT_API1(message_context, apikey)
+    message_context = get_message_context(message_history, have_chat_context, chat_with_history)
+    response = get_response_from_ChatGPT_API(message_context, apikey)
     message_history.append({"role": "assistant", "content": response})
     return response
 
 
-def handle_messages_get_response_stream1(message, apikey, message_history, have_chat_context, chat_with_history):
+def handle_messages_get_response_stream(message, apikey, message_history, have_chat_context, chat_with_history):
     message_history.append({"role": "user", "content": message})
-    message_context = get_message_context1(message_history, have_chat_context, chat_with_history)
-    generate = get_response_stream_generate_from_ChatGPT_API1(message_context, apikey)
+    message_context = get_message_context(message_history, have_chat_context, chat_with_history)
+    generate = get_response_stream_generate_from_ChatGPT_API(message_context, apikey)
     return generate
 
 
-@app.route('/returnMessage1', methods=['GET', 'POST'])
-def return_message1():
+@app.route('/returnMessage', methods=['GET', 'POST'])
+def return_message():
     """
     获取用户发送的消息，调用get_chat_response()获取回复，返回回复，用于更新聊天框
     :return:
@@ -204,13 +262,13 @@ def return_message1():
     apikey = API_KEY
 
     if STREAM_FLAG:
-        generate = handle_messages_get_response_stream1(send_message, apikey, messages_history, 12, chat_with_history)
+        generate = handle_messages_get_response_stream(send_message, apikey, messages_history, 12, chat_with_history)
         return app.response_class(generate(), mimetype='application/json')
     else:
-        generate = handle_messages_get_response1(send_message, apikey, messages_history, 12, chat_with_history)
+        generate = handle_messages_get_response(send_message, apikey, messages_history, 12, chat_with_history)
         return generate
 
 
 if __name__ == '__main__':
-    # app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000), debug=False)
-    app.run(debug=False, port=os.getenv("PORT", default=5000))
+    app.run(host="0.0.0.0", port=os.getenv("PORT", default=5000), debug=False)
+    # app.run(debug=False, port=os.getenv("PORT", default=5000))
